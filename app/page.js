74ec1fa346e23845001f08bca83b0a489711e893
wills-dev <ejioguvictor6@gmail.com>
2025-02-06 01:33:14 +0100

    added:  navbar  layout

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="font-bold">Hello world</h2>
+    <main className="min-h-screen relative">
+      <Navbar />
     </main>
   );
 }
