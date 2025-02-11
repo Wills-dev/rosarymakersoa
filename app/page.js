cfbb82cc4147b7e0ff42eecd728dc7649a4329aa
wills-dev <ejioguvictor6@gmail.com>
2025-02-11 13:39:15 +0100

    added

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import Navbar from "@/components/Navbar";
-import AboutUs from "@/components/AboutUs";
-import HeroSection from "@/components/HeroSection";
-import WhatWeDo from "@/components/WhatWeDo";
-import JoinUs from "@/components/JoinUs";
 import Image from "next/image";
-import MakeDonation from "@/components/MakeDonation";
-import Footer from "@/components/Footer";
-import OurLatestNews from "@/components/OurLatestNews";
+
+import Navbar from "../components/Navbar";
+import WhatWeDo from "../components/WhatWeDo";
+import JoinUs from "../components/JoinUs";
+import MakeDonation from "../components/MakeDonation";
+import Footer from "../components/Footer";
+import OurLatestNews from "../components/OurLatestNews";
+import HeroSection from "../components/HeroSection";
+import AboutUs from "../components/AboutUs";
 
 export default function Home() {
   return (
