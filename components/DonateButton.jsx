e83577c8694f501846501f302961f0c7a6ff1f0d
wills-dev <ejioguvictor6@gmail.com>
2025-02-11 12:15:09 +0100

    update

--- components/DonateButton.jsx
+++ components/DonateButton.jsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 const DonateButton = () => {
   return (
     <>
       {" "}
-      <button className="bg-primary-color py-3 px-8 text-white hover:bg-blue-900  transition-all duration-300 max-w-fit z-10">
+      <Link
+        href="/make-a-donation-2"
+        className="bg-primary-color py-3 px-8 text-white hover:bg-blue-900  transition-all duration-300 max-w-fit z-10"
+      >
         Donate
-      </button>
+      </Link>
     </>
   );
 };
