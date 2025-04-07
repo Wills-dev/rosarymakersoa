93bb397005b56bb8cc26b20ec40575134aba66f8
wills-dev <ejioguvictor6@gmail.com>
2025-04-08 00:01:10 +0100

    feat: hero section

--- components/MainSection.jsx
+++ components/MainSection.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const MainSection = () => {
   return (
     <section className="w-full  border-t-1 border-primary-brown">
-      <div className="md:h-[34rem] h-72 w-full bg-primary-black my-6"></div>
+      <div className="md:h-[34rem] h-72 w-full bg-primary-black my-6 bg-hero-img bg-cover bg-no-repeat relative">
+        <div className="absolute bg-primary-black opacity-80 top-0 left-0 w-full h-full" />
+        <div className="flex justify-center items-center w-full h-full">
+          <div className="max-w-2xl z-20 space-y-4">
+            <h1 className="text-5xl text-white t font-poppins text-center leading-tight  font-bold">
+              Enjoying the Dignity of Honest Human Labor in Rosary Making
+            </h1>
+            <p className="text-gray-300 text-center">
+              On a mission to empower adults with disabilities by providing
+              meaningful employment, enriching their lives through the dignity
+              of honest work.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
