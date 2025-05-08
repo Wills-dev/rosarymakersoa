608cd8dc55fc6c1314434b3b7140cdd214afe5d2
wills-dev <ejioguvictor6@gmail.com>
2025-05-08 15:12:05 +0100

    added: hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,11 @@
 import MainLayout from "../components/templates/MainLayout";
+import HeroSection from "../components/organisms/HeroSection";
 
 const page = () => {
   return (
-    <MainLayout>hello world</MainLayout>
-    // <main className="relative min-h-screen w-full bg-primary-color ">
-    //   <div className="padding-x w-full">
-    //     <Nav />
-    //     <MainSectuon />
-    //   </div>
-    //   <section className="bg-cream py-6 w-full">
-    //     <div className="padding-x w-full space-y-6">
-    //       <AboutUsSection />
-    //       <BePart />
-    //       <Prayer />
-    //       <CreateDonation />
-    //       <OurLatestNews />
-    //     </div>
-    //   </section>
-    //   <Footer />
-    // </main>
+    <MainLayout>
+      <HeroSection />
+    </MainLayout>
   );
 };
 
