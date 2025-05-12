b59867baef2a0f91aa69140a9b6bf2edef21149e
wills-dev <ejioguvictor6@gmail.com>
2025-05-12 21:06:34 +0100

    feat:  added image for donation section

--- components/molecules/cards/OurCard.jsx
+++ components/molecules/cards/OurCard.jsx
@@ -3,7 +3,7 @@ import OurIcon from "../../atoms/OurIcon";
 
 const OurCard = ({ our }) => {
   return (
-    <div className="w-full max-md:min-w-96 max-sm:min-w-full flex-1 rounded-lg p-6 space-y-3 group bg-black hover:bg-blue-900  transition-all duration-300 ease-out shadow">
+    <div className="w-full max-md:min-w-96 max-sm:min-w-full flex-1 rounded-lg p-6 space-y-3 group bg-black hover:bg-blue-950  transition-all duration-300 ease-out shadow">
       <OurIcon>{our?.icon}</OurIcon>
       <OurHeading title={our?.title} />
       <p className="text-white">{our?.description}</p>
