2e2d4498d64112cfea9700d3ec129fc939168631
wills-dev <ejioguvictor6@gmail.com>
2025-05-18 10:29:47 +0100

    added: make a donation content

--- app/make-a-donation-2/page.jsx
+++ app/make-a-donation-2/page.jsx
@@ -1,31 +1,31 @@
-import MainLayout from "../../components/MainLayout";
+import PageTitle from "../../components/atoms/PageTitle";
+import SectionParagraph from "../../components/atoms/SectionParagraph";
+import MainLayout from "../../components/templates/MainLayout";
 
 const MakeDonation = () => {
   return (
     <MainLayout>
-      {" "}
-      <section className="bg-cream py-6 w-full">
-        {" "}
-        <div className="padding-x w-full space-y-6">
-          <div className="section-bg text-primary-brown space-y-4">
-            <h3 className="sm:text-lg uppercase font-semibold text-primary-black border-b-1">
-              Make a Donation
-            </h3>
-            <div className="py-6 text-primary-brown space-y-4">
-              <p>
-                Rosary Makers of America is a 501(c)(3) nonprofit organization.
-                Please support our work. Our Mission is to get special needs
-                adults employed, to enrich and improve their lives. Be part of
-                the Rosary Makers of America family.
-              </p>
-              <p className="font-bold sm:text-lg">
-                Volunteer, make a donation or pray for us.
-              </p>
-              <p>
-                Donating to a Catholic Appeal or Church? Consider a
-                tax-deductible donation to support us!
-              </p>
-            </div>
+      <div className="sm:pt-16 pt-20">
+        <section className="padding-y padding-ctn">
+          <PageTitle title="Make a Donation" />
+        </section>
+      </div>
+      <section className="padding-y padding-ctn bg-gray-100">
+        <div className=" space-y-4">
+          <div className="space-y-4">
+            <SectionParagraph className="sm:text-xl  text-lg ">
+              Rosary Makers of America is a 501(c)(3) nonprofit organization.
+              Please support our work. Our Mission is to get special needs
+              adults employed, to enrich and improve their lives. Be part of the
+              Rosary Makers of America family.
+            </SectionParagraph>
+            <SectionParagraph className="font-bold sm:text-lg">
+              Volunteer, make a donation or pray for us.
+            </SectionParagraph>
+            <SectionParagraph className="sm:text-xl  text-lg ">
+              Donating to a Catholic Appeal or Church? Consider a tax-deductible
+              donation to support us!
+            </SectionParagraph>
           </div>
         </div>
       </section>
