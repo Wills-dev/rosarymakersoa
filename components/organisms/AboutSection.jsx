94e1f62e4e0a0db8c8126ef1c5829a751874e336
wills-dev <ejioguvictor6@gmail.com>
2025-07-04 12:41:41 +0100

    update: animated home page

--- components/organisms/AboutSection.jsx
+++ components/organisms/AboutSection.jsx
@@ -1,33 +1,26 @@
-import Image from "next/image";
-
+import bgImg from "/public/assets/images/about-us.JPG";
 import SectionTitle from "../atoms/SectionTitle";
 import AboutUsContent from "../molecules/AboutUsContent";
 import SectionLayout from "../templates/SectionLayout";
+import AnimatedImage from "../molecules/AnimatedImage";
 
 const AboutSection = () => {
   return (
-    <SectionLayout>
-      <div className="padding-ctn padding-y pt-0">
-        <div className="flex md:gap-16 gap-10 md:items-center max-md:flex-col">
-          <div className="max-w-xl w-full">
-            <SectionTitle title="About us" />
-            <div className="pt-10">
-              <AboutUsContent />
+    <div className="">
+      <AnimatedImage img={bgImg} />
+      <SectionLayout>
+        <div className="padding-ctn padding-y pt-0">
+          <div className="flex md:gap-16 gap-10 md:items-center max-md:flex-col">
+            <div className="max-w-xl w-full">
+              <SectionTitle title="About us" />
+              <div className="pt-10">
+                <AboutUsContent />
+              </div>
             </div>
           </div>
-          <div className="flex-1 w-full">
-            <Image
-              alt="about-us"
-              src="/assets/images/about-us.JPG"
-              width={400}
-              height={600}
-              priority
-              className="object-cover h-auto w-full"
-            />
-          </div>
         </div>
-      </div>
-    </SectionLayout>
+      </SectionLayout>
+    </div>
   );
 };
 
